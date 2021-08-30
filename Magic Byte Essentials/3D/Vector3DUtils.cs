b5105286c7bbccfe480c471cdf7f40ba52a0b195
Matheus Markies <matheus.markies@gmail.com>
2021-08-30 10:51:39 -0300

    Bezier Curves

--- Magic Byte Essentials/3D/Vector3DUtils.cs
+++ Magic Byte Essentials/3D/Vector3DUtils.cs
@@ -85,5 +85,24 @@ namespace EssentialMechanics.Logic
 
             return new Vector3(x, y, z);
         }
+        public static Vector3 quadraticBezierCurve(Vector3 a,Vector3 b,Vector3 c,float t)
+        {
+            Vector3 p0 = Vector3.Lerp(a,c,t);
+            Vector3 p1 = Vector3.Lerp(b, c, t);
+
+            return Vector3.Lerp(p0,p1,t);
+        }
+        public static Vector3 cubicBezierCurve(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
+        {
+            Vector3 p0 = Vector3.Lerp(a, c, t);
+            Vector3 p1 = Vector3.Lerp(b, d, t);
+            Vector3 p2 = Vector3.Lerp(c, d, t);
+
+            Vector3 p3 = Vector3.Lerp(p0, p2, t);
+            Vector3 p4 = Vector3.Lerp(p1, p2, t);
+
+            return Vector3.Lerp(p3, p4, t);
+        }
+
     }
 }
